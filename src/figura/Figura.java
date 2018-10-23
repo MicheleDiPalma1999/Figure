96002eee11337c532d440e84cba4ae66c23f8401
mikid <mikid@DESKTOP-6UM4TEU.home>
2018-10-23 10:57:48 +0200

    Commi grafico

--- src/figura/Figura.java
+++ src/figura/Figura.java
@@ -1,15 +1,22 @@
 package figura;
 
+import java.awt.Color;
+import java.util.*;
+
+import java.awt.*;
+import javax.swing.*;
+
 public class Figura {
 
     public static void main(String[] args) {
         test test1 = new test();
-        test1.test();
+        Scanner scan1 = new Scanner(System.in);
+        String ancora;
+        do {
+            test1.test();
+            System.out.println("--------------------------");
+            System.out.println("Uscire? [Exit]");
+            ancora = scan1.next();
+        } while (!ancora.toLowerCase().equals("exit"));
     }
-
-    
 }
-
-
-
-
